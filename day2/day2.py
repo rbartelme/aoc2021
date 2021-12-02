abfe916c7bb8c48acd30b1d8f4e5ab54081ba2bb
Ryan Bartelme <r.p.bartelme@gmail.com>
2021-12-02 21:40:30 +0000

    part1 done

--- day2/day2.py
+++ day2/day2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-
+input_df = f"/workspace/aoc2021/day2/input_day2.txt"
+df = pd.read_csv(input_df, sep=" ", header=None, names=["movement", "value"])
+#print(df.head())
+total_moves = df['value'].groupby(df['movement'])
+print(total_moves.sum())
+print((2063-973)*1868)
